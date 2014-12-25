1a2bb8e19c31c623951669463d3155b9ac43e429
rerdos <ricsi96@gmail.com>
2014-12-25 23:58:39 +0100

    Min and Max jump

--- beadando/tavolugras2.c
+++ beadando/tavolugras2.c
@@ -2,10 +2,12 @@
 
 int main(void) {
 
-int sorszam, rajtszam, rajtszam_hiba;
-int eredmeny, eredmeny_hiba, regi_eredmeny, ujra;
+int sorszam, rajtszam, max_rajtszam, min_rajtszam;
+float eredmeny, max_eredmeny, min_eredmeny, atlag_eredmeny;
 
 	sorszam = 0;
+	max_eredmeny = -1;
+	min_eredmeny = 11;
 
 	while(0 == 0){
 	
@@ -15,21 +17,36 @@ int eredmeny, eredmeny_hiba, regi_eredmeny, ujra;
 		if (rajtszam < 0 || rajtszam > 20) {
 			printf("Hiba! Nem 1 es 20 kozotti rajtszam lett megadva!");
 			return (0);
-		} else if (0 == 1){
-		
-		
-		
-		} else {
-			
-			printf("\nKerem adja meg a(z) %d. versenyzo eredmenyet 0.0 es 10.0 kozott: ", sorszam+1);
-			scanf("%d", &eredmeny);
-
-			if (eredmeny < 0.0 || eredmeny > 10.0) {
-				printf("Hiba! Nem 0.0 es 10.0 kozotti eredmeny lett megadva!");
+		} else if (rajtszam == 0){
+			if (max_eredmeny == -1){
+				printf("Nem lett versenyzo megadva!");
 				return (0);
 			}
+			printf("\nA legjobb ugrast a %d. rajtszamu versenyzo ugrotta %fm-es eredmennyel!",max_rajtszam , max_eredmeny);
+			printf("\nA legrosszabb ugrast a %d. rajtszamu versenyzo ugrotta %fm-es eredmennyel!",min_rajtszam , min_eredmeny);
+			return (0);
+		
 		}
-	sorszam++;
+		printf("Kerem adja meg a(z) %d. versenyzo eredmenyet 0.0 es 10.0 kozott: ", sorszam+1);
+		scanf("%f", &eredmeny);
+
+		if (eredmeny < 0.0 || eredmeny > 10.0) {
+			printf("Hiba! Nem 0.0 es 10.0 kozotti eredmeny lett megadva!");
+			return (0);
+			
+		}
+		
+		if (max_eredmeny < eredmeny){
+			max_eredmeny = eredmeny;
+			max_rajtszam = rajtszam;
+		}
+		
+		if (min_eredmeny > eredmeny){
+			min_eredmeny = eredmeny;
+			min_rajtszam = rajtszam;
+		}
+		
+		sorszam++;
 	}
 return (0);
 }
