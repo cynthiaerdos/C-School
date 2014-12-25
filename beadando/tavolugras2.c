01cf138fa7451d27c8457ca7ab2caf1af1d7363a
rerdos <ricsi96@gmail.com>
2014-12-25 21:12:40 +0100

    Reworked again

--- beadando/tavolugras2.c
+++ beadando/tavolugras2.c
@@ -5,50 +5,31 @@ int main(void) {
 int sorszam, rajtszam, rajtszam_hiba;
 int eredmeny, eredmeny_hiba, regi_eredmeny, ujra;
 
-sorszam = 0;
-rajtszam_hiba = 0;
-eredmeny_hiba = 0;
+	sorszam = 0;
 
-
-		while(0 == 0){
-		
-			if (rajtszam_hiba == 1) {
-				printf("Hiba! Nem 1 es 20 kozotti rajtszam lett megadva!");
-			}
-		
+	while(0 == 0){
+	
 		printf("\nKerem adja meg a(z) %d. versenyzo rajtszamat 1 es 20 kozott: ", sorszam+1);
 		scanf("%d", &rajtszam);
-		
-			if (rajtszam < 0 || rajtszam > 20) {
-				rajtszam_hiba = 1;
-			} else {
-				rajtszam_hiba = 0;
-				
-						
-						ujra == 0;
-						
-						if (eredmeny_hiba == 1) {
-							printf("Hiba! Nem 0.0 es 10.0 kozotti eredmeny lett megadva!");
-						}
-					
-					printf("\nKerem adja meg a(z) %d. versenyzo eredmenyet 0.0 es 10.0 kozott: ", sorszam+1);
-					scanf("%d", &eredmeny);
-					
-						if (eredmeny < 0.0 || eredmeny > 10.0) {
-							eredmeny_hiba = 1;
-						} else {
-							eredmeny_hiba = 0;
-							sorszam++;
-							if (rajtszam == 0){
-								return (0);
-							}
-						}
-					}
-			}
-		
-		}
-	
 
-	
-	return (0);
+		if (rajtszam < 0 || rajtszam > 20) {
+			printf("Hiba! Nem 1 es 20 kozotti rajtszam lett megadva!");
+			return (0);
+		} else if (0 == 1){
+		
+		
+		
+		} else {
+			
+			printf("\nKerem adja meg a(z) %d. versenyzo eredmenyet 0.0 es 10.0 kozott: ", sorszam+1);
+			scanf("%d", &eredmeny);
+
+			if (eredmeny < 0.0 || eredmeny > 10.0) {
+				printf("Hiba! Nem 0.0 es 10.0 kozotti eredmeny lett megadva!");
+				return (0);
+			}
+		}
+	sorszam++;
+	}
+return (0);
 }
